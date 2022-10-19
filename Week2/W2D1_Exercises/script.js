ad646279713777ec0dee932501d37575343f323b
Yaniv Maximov <ymaximov@YanivEmmix.local>
2022-10-19 13:30:08 +0300

    working on w2 exercises

---
+++ Week2/W2D1_Exercises/script.js
@@ -0,0 +1,191 @@
+// Ex.1
+
+// window.console.log('yaniv') 
+// console.log(hello')
+// alert('hi')
+// confirm('yaniv')
+// var word = "hello"
+const word = "dinner"
+const word2 = "hamburger"
+let sentence=`If your favorite food is ${word2}, 
+and your favorite meal of the day is ${word} ,
+I eat ${word2} at every ${word}`
+console.log(sentence)
+
+console.log(word, sentence, word)
+console.log("If your favorite food is chocolate", word, "and your favorite meal of the day is", word)
+
+//Ex.2
+const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+const one= myWatchedSeries.length //number of values within the brackets
+console.log(myWatchedSeries[0].length) // number of characters in line 0
+console.log(myWatchedSeries)
+
+//Ex.3
+let celcius =30
+console.log(celcius/5*9+32)
+// let usercelcius = +prompt("choose celcius")
+// console.log(usercelcius/5*9+32)
+
+// Ex.4
+let c;
+    let a = 34;
+    let b = 21;
+
+    console.log(a+b) //first expression
+    // Prediction:55
+    // Actual:55
+    console.log("Prediction:55, Actual:",a+b)
+
+    a = 2;
+
+    console.log(a+b) //second expression
+    // Prediction:23
+    // Actual:23
+console.log("Prediction:23, Actual:",a+b)
+console.log(c)
+console.log(3 + 4 + '5'); // purpose of the ' is to not include whats inside in the math equation but to add it to the end of the solution
+
+// ex.5
+typeof(15)
+// Prediction: number
+// Actual:
+
+typeof(5.5)
+// Prediction: number
+// Actual:
+
+typeof(NaN)
+// Prediction: undefined
+// Actual:
+
+typeof("hello")
+// Prediction: string
+// Actual:
+
+typeof(true)
+// Prediction: boolean
+// Actual:
+
+typeof(1 != 2)
+// Prediction: false
+// Actual:
+
+"hamburger" + "s"
+// Prediction: hamburgers
+// Actual:
+console.log()
+"hamburgers" - "s"
+// Prediction: hamburger
+// Actual:
+console.log("hamburgers" - "s")
+
+"1" + "3"
+// Prediction: 13
+// Actual: 
+
+"1" - "3"
+// Prediction:
+// Actual: NAN
+
+"johnny" + 5
+// Prediction:
+// Actual: johnny5
+
+"johnny" - 5
+// Prediction: NAN
+// Actual: 
+
+99 * "hello"
+// Prediction:
+// Actual: NAN
+
+1 != 1
+// Prediction: False
+// Actual: 
+
+1 == "1"
+// Prediction: True
+// Actual:
+console.log(1 == "1")
+1 === "1"
+// Prediction: True
+// Actual: False
+console.log(1 === "1")
+
+5 + "34"
+// Prediction: 534
+// Actual:
+
+5 - "4"
+// Prediction: NaN
+// Actual:
+
+10 % 5
+// Prediction: 
+// Actual:0
+console.log(10 % 5)
+5 % 10
+// Prediction:
+// Actual:5
+console.log(5 % 10)
+"Java" + "Script"
+// Prediction: javascript
+// Actual:
+
+" " + " "
+// Prediction:  
+// Actual:
+
+" " + 0
+// Prediction:
+// Actual: 0
+
+true + true
+// Prediction: NAN
+// Actual:
+
+true + false
+// Prediction: NAN
+// Actual:
+
+false + true
+// Prediction: NAN
+// Actual:
+
+false - true 
+// Prediction: NAN
+// Actual:
+
+!true
+// Prediction: false
+// Actual:
+
+3 - 4
+// Prediction: -1
+// Actual:
+
+"Bob" - "bill"
+// Prediction: NAN
+// Actual:
+
+
+
+
+
+
+
+
+
+
+
+// mathematical equations //
+let num = 4
+let num1 = 1
+console.log(num+num1)
+
+num +=1
+console.log(num)
+
+num -=1
+console.log(num)
