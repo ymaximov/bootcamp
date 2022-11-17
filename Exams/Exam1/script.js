8f44cb747163663f29e50c4b92912c61d90f4277
ymaximov <ymaximov@yahoo.com>
2022-11-17 13:33:42 +0200

    test

---
+++ Exams/Exam1/script.js
@@ -0,0 +1,130 @@
+// 1. nums[1]
+
+// 2. Nothing would happen. array stays the same
+
+// 3. const favoriteNumbers = [4, 5, 6, 1, 2, 3, 7, 8, 9]
+
+// 4. Real Madrid
+
+// 5. const football = {
+//     type: "sport",
+//     clubs: {
+//       france : "Paris Saint-Germain",
+//       spain : "Atlético Madrid",
+//       myteam: France national football team
+//     }
+//   }  
+  
+
+// 6. yes can be defined. Invoking the function:
+//     x(1, 2) would = 2 or whatever numbers are in parenthesis
+
+// 7. No, since they are unnamed they cannot be requestIdleCallback.apply
+
+// 8. Yes. Returns Hello Javascript
+
+// 9. const myFunction = (num1, num2)=> num1 + num2
+//   myFunction(40, 2)
+//   myFunction(40, 0)
+//   console.log("the answer to everything is", myFunction(42,0))
+
+
+// 10. Prints Poppin' Bottles to the console.
+// the foo function invokes the bar function when foo is logged bc bar is what was set to be retuned.France
+
+// 11. click
+
+// 12. yes
+
+// // 13. 
+// const button = document.getElementById("btn");
+// console.log(button);
+// const para = document.getElementsByTagName("p")
+// console.log(para);
+
+//  const chgColor = (event)=>{
+//     event.target.style.color = "red"
+//  }
+
+// button.addEventListener('click',()=>{
+//    const newP = document.createElement("p")
+//    const newContent = document.createTextNode("New Paragraph")
+//    newP.appendChild(newContent)
+//    const element = document.body
+//    element.appendChild(newP)
+//    newP.addEventListener('mouseover', chgColor)
+        
+// });
+
+
+// para.addEventListener('mouseover', function ()=>{  
+//     para.style.color = 'red';
+// });
+
+// 14.
+// const marks = [67, 60, 89, 90, 67, 42];
+// let sum = 0;
+// for (let i = 0; i <= marks.length; i++){
+//     sum += marks[i]
+// }
+
+// 15. returns all "marks" greater than or equal to 50
+
+// 16. returns only 50, 88
+
+// 17. ??
+
+18. 
+// const secondInput = [ 1, -4, 12, 0, -3, 29, -150];
+// let sum = 0
+//  for (let i = 0; i <= secondInput.length; i++){
+//     if (i>=0){
+//         sum += secondInput[i]
+//     } 
+//  }
+// console.log(sum)
+
+// 19.
+// const input = 'George Raymond Richard Martin';
+// const myArray = input.split(" ")
+// myArray.forEach(func=> console.log(func.charAt(0)))
+
+20.
+
+21.
+// function changeTshirt (myshirt){
+//     console.log("myshirt", myshirt);
+//     myshirt.color = "red"
+//     console.log("myshirt", myshirt); //this is the parameter
+//     console.log("tshirt", tshirt);   //this is the global variable
+// }
+
+
+// const tshirt = {
+//     color : "blue",
+//     price : 10
+// }
+
+// changeTshirt(tshirt)
+
+// 22.
+// const football = {
+//     type: "sport",
+//     clubs: {
+//       france : "Paris Saint-Germain",
+//       spain : "Atlético Madrid",
+//     }
+//   }  
+// const {football.clubs.france}
+
+// 23 DESTRUCTURING
+
+// 24. Chocolate Cake is for $10
+
+// 25. Data will be printed in upper case to the console and the promise is fulfilled
+
+// 26. All the data will be logged to the console
+
+// 27. "first" will return
+
+
