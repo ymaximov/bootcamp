f276fd3767468c1c9b5bc5dd9c1d9a4de7448aa6
ymaximov <ymaximov@yahoo.com>
2022-11-19 18:10:46 +0200

    upload

--- Exams/Exam1/script.js
+++ Exams/Exam1/script.js
@@ -61,28 +61,34 @@
 //     para.style.color = 'red';
 // });
 
-// 14.
+14.
 // const marks = [67, 60, 89, 90, 67, 42];
-// let sum = 0;
-// for (let i = 0; i <= marks.length; i++){
-//     sum += marks[i]
-// }
+// // let sum = 0;
+// // for (let i = 0; i <= marks.length; i++){
+// //     sum += marks[i]
+// // }
 
-// 15. returns all "marks" greater than or equal to 50
+// for (const mark of marks)
+// console.log(mark);
+
+
+// 15. returns every mark of the marks array that "passed the test" and are greater or equal to 50 ([67, 60, 89, 90, 67] excluding 42)
 
 // 16. returns only 50, 88
 
-// 17. ??
+// // 17. 
+// const input = [ 1, 2, 3, 4, 5 ];
+// const squared = input.map(x => x * x)
+// console.log(squared);
 
 18. 
 // const secondInput = [ 1, -4, 12, 0, -3, 29, -150];
-// let sum = 0
-//  for (let i = 0; i <= secondInput.length; i++){
-//     if (i>=0){
-//         sum += secondInput[i]
-//     } 
-//  }
-// console.log(sum)
+// let initialValue = 0
+// let posNum = secondInput.filter(num => num >= 0)
+// const sumArray = posNum.reduce((accumulator, currentValue) => accumulator + currentValue,
+// initialValue)
+// console.log(posNum);
+// console.log(sumArray)
 
 // 19.
 // const input = 'George Raymond Richard Martin';
@@ -115,10 +121,27 @@
 //       spain : "Atlético Madrid",
 //     }
 //   }  
-// const {football.clubs.france}
+//   let { clubs } = football
+//   console.log(clubs);
 
-// 23 DESTRUCTURING
+// 23 
+const football = {
+    type: "sport",
+    clubs: {
+      france : "Paris Saint-Germain",
+      spain : "Atlético Madrid",
+    }
+  }  
 
+let { clubs: { france }, clubs: { spain } } = football
+console.log(france);
+console.log(spain);
+
+function retrieveSports(team1, team2) {
+    const sentence = `My favorite football teams are ${franceTeam} and ${spainTeam}`;
+    console.log(france, spain);
+ }
+ retrieveSports(football)
 // 24. Chocolate Cake is for $10
 
 // 25. Data will be printed in upper case to the console and the promise is fulfilled
