9fed1e9ed05a0649ec58e0771290e513b6468486
Yaniv Maximov <yanivmaximov@icloud.com>
2022-12-07 21:09:12 +0200

    1

---
+++ Part_time/Week9/Day2/XP/script.js
@@ -0,0 +1,38 @@
+// //EX2
+// const xhr =  new XMLHttpRequest();
+
+// xhr.onload = function(){
+//     if (this.status === 200){
+//         try {
+//             const resObj = JSON.parse(this.responseText);
+//             console.log(resObj);
+//         } catch(e){
+//             console.warn("Error in JSON. Could Not Parse");
+//         }
+//     } else {
+//         console.warn("Did Not Receive 200 Status");
+//     }
+// }
+
+// xhr.open("GET", "https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My");
+// xhr.send();
+
+
+//EX2 10 gifs about the sun
+// const xhr =  new XMLHttpRequest();
+
+// xhr.onload = function(){
+//     if (this.status === 200){
+//         try {
+//             const resObj = JSON.parse(this.responseText);
+//             console.log(resObj);
+//         } catch(e){
+//             console.warn("Error in JSON. Could Not Parse");
+//         }
+//     } else {
+//         console.warn("Did Not Receive 200 Status");
+//     }
+// }
+
+// xhr.open("GET", "https://api.giphy.com/v1/gifs/search?api_key=k5oZLRzjZAXakdbQXAo5CqyXCcjX1rFL&q=sun&limit=10&offset=0&rating=g&lang=en");
+// xhr.send();
