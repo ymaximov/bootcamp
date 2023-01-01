b9d6644cf33a045d1c30cd759835e6cb8b848f53
Yaniv Maximov <yanivmaximov@icloud.com>
2023-01-01 16:06:18 +0200

    2

--- Part_time/Week12/Day2/XP/ExercisesXP.sql
+++ Part_time/Week12/Day2/XP/ExercisesXP.sql
@@ -35,4 +35,25 @@ where jobs.job_title != '%Manager%'
 select max_salary from jobs
 order by max_salary desc
 
+-- JOINS
 
+-- 1
+select departments.department_id, locations.location_id, locations.street_address, locations.city, locations.state_province, locations.country_id
+from departments
+inner join locations
+on locations.location_id=departments.location_id
+
+-- 2
+select employees.first_name, employees.last_name, departments.department_name, departments.department_id
+from employees
+inner join departments
+on employees.department_id=departments.department_id
+
+-- 3
+select employees.first_name, employees.last_name, departments.department_name, employees.employee_id
+from employees
+inner join departments
+on
+
+
+select * from departments
