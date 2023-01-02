2f5fb91902ac9f1ce727b552bc6d1084a7e00c86
Yaniv Maximov <yanivmaximov@icloud.com>
2023-01-02 14:22:55 +0200

    XP Daily

---
+++ Part_time/Week13/Day1HW/XP/npm-node/main.js
@@ -0,0 +1,20 @@
+//EX1
+
+const a = 5;
+const b = 10;
+
+console.log(`Hello you are ${a + b}`);
+
+//EX2
+
+console.log('Hi How are you');
+
+//EX3
+
+const a = 5;
+const b = 10;
+let x;
+const c = 6;
+x = a + b;
+
+console.log(x);
